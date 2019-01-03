45a3aa635a1aba095982ecaeb6e9c61885750f96
BNgoum <benjamin.ngoum@gmail.com>
2019-01-03 10:28:25 +0100

    FIX - Change label in user model for bet room owner

--- Models/user.model.js
+++ Models/user.model.js
@@ -22,7 +22,7 @@ const userSchema = new Schema({
         recipient: [{ type: Schema.Types.ObjectId }]
     },
     bet_room: {
-        creator: [{ type: Schema.Types.ObjectId }],
+        owner: [{ type: Schema.Types.ObjectId }],
         participant: [{ type: Schema.Types.ObjectId }]
     }
 })
